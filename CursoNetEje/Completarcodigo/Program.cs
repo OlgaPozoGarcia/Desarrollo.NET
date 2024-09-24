71f9a88bbb7186731f3b29de99d2e1d2f5ec6d84
OlgaPozoGarcia . <opozogarcia@gmail.com>
2024-09-24 13:02:04 +0200

    Carpeta añadida ejercicios

---
+++ CursoNetEje/Completarcodigo/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Modificar para incorporar las siguientes características:    
+// Indicar el numero de intentos que le quedan
+// Indicar si el número a adivinar es mayor o menor que el introducido
+
+//Pedir al usuario que ingrese un número a adivinar
+Console.WriteLine("Introduce un número para que otro jugador lo adivine");
+int numeroAdivinar = int.Parse(Console.ReadLine());
+
+//Limpiar la consola para que el segundo juguador no vea el número
+Console.Clear();
+
+//Dar tres oportunidades para adivinar el número
+for (int i = 0; i < 2; i++)
+    {
+    Console.Write("Adivina el número:");
+    int intento = int.Parse(Console.ReadLine());
+
+    if (intento == numeroAdivinar) 
+    {
+        Console.WriteLine("¡Felicidades! Has adivinado el número.");
+        return;
+      }
+    else
+    {
+        if (intento < numeroAdivinar) 
+        {
+            Console.WriteLine("El número a adivinar es mayor");
+        }
+        else
+        {
+            Console.WriteLine("El número a adivinar es menor.");
+        }
+            Console.WriteLine("Número incorrecto. Intenta de nuevo.");
+        }
+     }
+   
---
+++ CursoNetEje/MayorEdad/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.Design;
+
+namespace MayorEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, Mundo");
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Por favor, ingresa tu edad");
+            string  edadPersonaConsola = Console.ReadLine();
+
+            // Convetir string a entero con Convert
+            // Convertir (Convert) a entero (ToInt32)
+            //      lo que has pedido por pantalla (edadPersonaConsola)
+            int edadPersona = Convert.ToInt32(edadPersonaConsola);
+
+            // int             int
+            if (edadPersona >= 18) 
+            {
+                Console.WriteLine("Eres mayor de edad.");
+        }
+            else 
+            {
+                Console.WriteLine("Eres menor de edad.");
+
+            //Determinar etapa de la vida
+
+            if (edadPersona <= 12)
+                {
+                  
+                    Console.WriteLine("Estas en la etapa: Niño");
+                    }
+            else if (edadPersona >= 12 && edadPersona <18) 
+                {
+                    Console.WriteLine("Estas en la etapa: adolescente");
+                     }
+            else if (edadPersona >= 18 && edadPersona <= 65)
+                { 
+                        Console.WriteLine("Estas en la etapa: Adulto");
+                    }
+            else if (edadPersona >= 65)
+                    Console.WriteLine("Estas en la etapa: Aduto Mayor");
+
+            
+        } // Fin del else
+     } //Fin del método Main
+   } // Fin del método Program
+} //Fin del namespace
+
+

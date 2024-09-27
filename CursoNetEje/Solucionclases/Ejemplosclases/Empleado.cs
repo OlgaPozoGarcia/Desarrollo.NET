4cce178ecf647aad89cd2ceeb8c018a3406535a5
OlgaPozoGarcia . <opozogarcia@gmail.com>
2024-09-27 12:20:39 +0200

    Nuevos cambios

---
+++ CursoNetEje/Solucionclases/Ejemplosclases/Empleado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EjemploClases
+{
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+
+
+
+        public string Departamento { get; set; }
+
+
+        public string Telefono { get; set; }
+
+        public string NumeroEmpleado { get; set; }
+
+        public string NSS { get; set; }
+
+        public string IBAN { get; set; }
+
+        //Metodo / (Funcion) MostarDatosEmpleado
+        public void MostrarDatosEmpleado()
+        {
+            //Mediante interpolacion de cadenas ($)
+            Console.WriteLine($"Nombre empleado:{Nombre} Departamento Empleado:{Departamento} Telefono Empleado:{Telefono}");
+
+
+
+            }
+        }
+}
+
--- CursoNetEje/Solucionclases/Ejemplosclases/Program.cs
+++ CursoNetEje/Solucionclases/Ejemplosclases/Program.cs
@@ -49,33 +49,47 @@ BentleyBentayga.MotorCoche.Combustible = "Diesel";
 
 //Crear las instancias de los empleados: Pedro, Maria y Juan
 
-Empleados empleado1 = new Empleados();
+Empleado empleado1 = new Empleado();
 
 empleado1.Nombre = "Pedro";
 empleado1.Departamento = "IT";
 
-Empleados empleado2 = new Empleados();
+empleado1.MostrarDatosEmpleado();
+
+Empleado empleado2 = new Empleado();
 
 empleado2.Nombre = "Maria";
 empleado2.Departamento = "Desarrollo Apps";
 
-Empleados empleado3 = new Empleados();
+empleado2.MostrarDatosEmpleado();
+
+Empleado empleado3 = new Empleado();
 
 empleado3.Nombre = "Juan";
 empleado3.Departamento = "Recursos Humanos";
 
-Empleados empleado4 = new Empleados();
+empleado3.MostrarDatosEmpleado();
+
+Empleado empleado4 = new Empleado();
 
 empleado4.Nombre = "Claudia";
 empleado4.Departamento = "Ventas";
+empleado4.Telefono = "658987421";
+empleado4.Telefono = "6987456321458";
 
-Empleados empleado5 = new Empleados();
+empleado4.MostrarDatosEmpleado();
+
+Empleado empleado5 = new Empleado();
 
 empleado5.Nombre = "Olga";
 empleado5.Departamento = "Marketing";
 empleado5.Telefono = "697806648";
 empleado5.NSS = "866515226655";
 empleado5.IBAN = "562158126425";
+
+empleado5.MostrarDatosEmpleado();
+
+
 
 
 
